9691bfe7e65b5b27d7df1d10d3bb53c239eaa870
Luis Camilo Garcia <lgarcia.training@wowdesarrollos.com>
2022-12-14 09:35:15 -0500

    Select mostrano input de uno o dos jugadores y guardando en consola

--- src/registro/registro.jsx
+++ src/registro/registro.jsx
@@ -5,39 +5,68 @@ import "../registro/Registro.css"
 
 function Registro() {
     const [user, setUser] = useState()
+    const [ingresoUsuario, setIngresoUsuario] = useState()
+
+    const saveData = (e) => {
+        const data = []
+        e.preventDefault()
+
+        console.log(e.target.select.value)
+
+        if (e.target.select.value === 'Un jugador') {
+            console.log(e.target.nick.value)
+        } else if (e.target.select.value === 'Dos jugadores') {
+            console.log(e.target.nick1.value)
+            console.log(e.target.nick2.value)
+        }
+
+    }
 
     function numeroJugadores(e) {
         setUser(e.target.value)
+        // console.log(e.target.value)
+    }
+
+    function ingresoUsuarios(e) {
+        setIngresoUsuario(e.target.value)
         console.log(e.target.value)
     }
 
     return (
         <div className="container">
-            <p><select value={user} onChange={numeroJugadores}>
-                <option>Elige el numero de jugadores</option>
-                <option>Un jugador</option>
-                <option>Dos jugadores</option>
 
-            </select></p>
+            <form value={ingresoUsuario} onSubmit={saveData}>
+                <p><select value={user} name="select" onChange={numeroJugadores}>
+                    <option>Elige el numero de jugadores</option>
+                    <option>Un jugador</option>
+                    <option>Dos jugadores</option>
 
-            {
-                user === 'Un jugador' && (
-                    <div>
-                        <input type="text" />
-                    </div>
-                )
-            }
+                </select></p>
 
-            {
-                user === 'Dos jugadores' && (
-                    <div>
-                        <input type="text" />
-                        <input type="text" />
-                    </div>
-                )
-            }
+                {
+                    user === 'Un jugador' && (
+                        <div>
+                            <p>Ingresa tu nick<input name="nick" type="text" /></p>
+                        </div>
+                    )
+                }
+
+                {
+                    user === 'Dos jugadores' && (
+                        <div>
+                            <p>Ingresa tu nick<input name="nick1" type="text" /></p>
+                            <p>Ingresa tu nick<input name="nick2" type="text" /></p>
+
+                        </div>
+                    )
+                }
+
+                <button>Login</button>
+            </form>
+
         </div>
     )
+
 }
 
 export default Registro;
