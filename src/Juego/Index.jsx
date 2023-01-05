634e43a7ef6ecf37f60665045f9be1a6194335b9
Franco Muñoz <francomunoz190@gmail.com>
2023-01-05 09:22:45 -0500

    tablero

---
+++ src/Juego/Index.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import Tablero from "./Tablero/Tablero";
+import { useState } from "react";
+const winnerPositions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+const Index = () => {
+  const [turn, setTurn] = useState("X");
+  const [squares, setSquares] = useState(Array(9).fill(null));
+/*   const [winnerPositions, setWinnerPositions] = useState([]); */
+  const [score, setScore] = useState({
+    X: 0,
+    O: 0,
+  });
+  const checkWinner = (newSquares) => {
+    for (let i = 0; i < winnerPositions.length; i) {
+      const [a, b, c] = winnerPositions[i];
+      if (
+        newSquares[a] &&
+        newSquares[a] === newSquares[b] &&
+        newSquares[a] === newSquares[c]
+      ) {
+        // HAY UN GANADOR
+        finGame(newSquares[a], winnerPositions[i])
+        return;
+      }
+    }
+    if (!newSquares.includes(null)) {
+      // HAY UN EMPATE
+      finGame(null, Array.from(Array(10).keys()))
+      return
+    }
+    setTurn(turn === "X" ? "O" : "X");
+  };
+  const handleClick = (square) => {
+    let newSquares = [...squares];
+    newSquares.splice(square, 1, turn);
+    setSquares(newSquares);
+    checkWinner(newSquares);
+  };
+  const finGame = (resul, winnerPositions) => {
+    setTurn(null);
+
+  };
+  return (
+    <div>
+      <Tablero turn={turn} squares={squares} onClick={handleClick} />
+    </div>
+  );
+};
+
+export default Index;
---
+++ src/Juego/Tablero/Tablero.jsx
@@ -0,0 +1,33 @@
+
+import Square from '../Square/Square';
+import './Tablero.css';
+
+const Tablero = ({squares, onClick, turn }) => {
+
+    const createSquares = values => (
+        values.map( value => (
+            <Square
+                turn={turn}
+                onClick={() => onClick(value)}
+                value={squares[value]}
+                key={`square_${value}`}
+            />
+        ))
+    );
+
+    return (
+        <div className="board">
+            <div className="row">
+               {createSquares([0,1,2])}
+            </div>
+            <div className="row">
+                {createSquares([3,4,5])}
+            </div>
+            <div className="row">
+                {createSquares([6,7,8])}
+            </div>
+        </div>
+    );
+}
+
+export default Tablero;
